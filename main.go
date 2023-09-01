eff659db0527c9cf359b26e8f3d680d9f55d6dc6
Jeremy Hager <47301461+jeremyhager@users.noreply.github.com>
2023-09-01 16:36:53 -0700

    creating template file

--- main.go
+++ main.go
@@ -5,14 +5,16 @@ import (
 	"os"
 	"text/template"
 
+	"github.com/jeremyhager/pokeapi/evolutionchains"
 	"github.com/jeremyhager/pokeapi/pokemon"
 	"github.com/jeremyhager/pokeapi/pokemonspecies"
 )
 
 func main() {
 	type pokemonInfo struct {
-		Pokemon pokemon.Pokemon
-		Species pokemonspecies.PokemonSpecies
+		Pokemon   pokemon.Pokemon
+		Species   pokemonspecies.PokemonSpecies
+		Evolution evolutionchains.EvolutionChain
 	}
 
 	poke, err := pokemon.Get("bulbasaur")
@@ -25,9 +27,17 @@ func main() {
 		log.Fatal(err)
 	}
 
+	chainId := string(speciesInfo.EvolutionChain.Url[len(speciesInfo.EvolutionChain.Url)-2])
+
+	chain, err := evolutionchains.Get(chainId)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	pokedex := pokemonInfo{
-		Pokemon: poke,
-		Species: speciesInfo,
+		Pokemon:   poke,
+		Species:   speciesInfo,
+		Evolution: chain,
 	}
 
 	tmplFile, err := template.ParseFiles("website/docs/gen2/pokemon.tmpl")
